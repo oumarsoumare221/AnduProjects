447c033d5794ec09290cce6a6b0a58d78025a4e5
smsow <sicksow12@gmail.com>
2024-07-10 17:00:47 +0000

    Fully responsive, just styling and height issues

--- andu-app/resources/views/history.blade.php
+++ andu-app/resources/views/history.blade.php
@@ -3,102 +3,14 @@
 
 <head>
     <title>History and Buffer Timelines</title>
-    <style>
-        body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            background: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            color: #333;
-        }
+    <link rel="stylesheet" href="{{ url('CSS/landingpage.css') }}">
 
-        .container {
-            background: rgba(255, 255, 255, 0.9);
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            width: 80%;
-            max-width: 1000px;
-            padding: 20px;
-            margin: 20px;
-        }
-
-        .historyContainer {
-            padding: 20px 0;
-        }
-
-        .HistoryLine {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 100%;
-            height: 2px;
-            background: #636363;
-            position: relative;
-        }
-
-        .historyPoints {
-            width: 15px;
-            height: 15px;
-            background: #000;
-            border-radius: 50%;
-            position: relative;
-        }
-
-        .historyPoints::after {
-            content: attr(data-year);
-            position: absolute;
-            top: 20px;
-            left: 50%;
-            transform: translateX(-50%);
-            font-size: 12px;
-            color: #333;
-        }
-
-        .HistoryTable {
-            width: 100%;
-            margin-top: 20px;
-            border-collapse: collapse;
-        }
-
-        .HistoryTable th,
-        .HistoryTable td {
-            padding: 10px;
-            text-align: left;
-        }
-
-        .HistoryTable th {
-            background: #124366;
-            color: #fff;
-            font-weight: normal;
-        }
-
-        .HistoryTable td {
-            background: #f9f9f9;
-            border-bottom: 1px solid #ddd;
-        }
-
-        .HistoryTable .mois p {
-            background: #D5EBFF;
-            border-radius: 4px;
-            text-align: center;
-            padding: 5px 10px;
-        }
-
-        .TableInfo a {
-            color: #2C4BFF;
-            text-decoration: none;
-        }
-    </style>
 </head>
 
 <body>
     <div class="container">
         @if(isset($histories) && isset($bufferTimelines))
-        <div class="historyContainer">
+        {{-- <div class="historyContainer">
             <div class="HistoryLine">
                 @foreach($histories as $history)
                 <div class="historyPoints" data-year="{{ $history->year }}"></div>
@@ -126,7 +38,220 @@
                     </tr>
                 </tbody>
             </table>
-        </div>
+        </div> --}}
+        <div class="historyContainer">
+            <div class="HistoryLine">
+                <div class="historyPoints"></div>
+                <div class="historyPoints"></div>
+                <div class="historyPoints"></div>
+                <div class="historyPoints"></div>
+                <div class="historyPoints"></div>
+                <div class="historyPoints"></div>
+                <div class="historyPoints" style="background-color: transparent;"></div>
+            </div>
+            <div class="spaceBetween"></div>
+            <table class="HistoryTable">
+                <thead>
+                    @foreach($histories as $history)
+                    <th class="mois" scope="col"> <p>{{ $history->month }}</p></th>
+                    @endforeach
+            
+                    <th class="mois" scope="col"><p><</p></th>
+                    <th class="mois" scope="col"><p>January</p></th>
+                    <th class="mois" scope="col"><p>August</p></th>
+                    <th class="mois"  scope="col"><p>December</p></th>
+                    <th class="mois" scope="col"><p>December</p></th>
+                </thead>
+                <tbody>
+                    <tr class="TableInfo">
+                 
+                        @foreach($histories as $history)
+                        <td>
+                            {{ $history->event }}<br>
+                            <a href="#">Read more</a>
+                        </td>
+                        @endforeach
+        
+                        <td>
+                            @foreach($histories as $history)
+                            <td>
+                                {{ $history->event }}<br>
+                                <a href="#">Read more</a>
+                            </td>
+                            @endforeach
+                            <div class="dotted">dbfb</div>
+                        </td>
+                        <td>
+                            Our founders had visa
+                            troubles and started
+                            traveling while growing
+                            Buffer, the team grew to
+                            7 people by August  <br>
+                            <a href="">Read more</a>
+                        </td>
+                        <td>
+                            Buffer's first all-company
+                            retreat took place in
+                            Lake Tahoe  <br>
+                            <a href="">Read more</a>
+                            <div class="dotted">dbfb</div>
+                            <div class="dotted">dbfb</div>
+                        </td>
+                        <td>
+                            We raised $3.5M in
+                            Series A funding  <br>
+                            <a href="">Read more</a>
+                        </td>
+                        <td>
+                            We acquired Respondly  <br>
+                            <a href="">Read more</a>
+                        </td>
+                      </tr>\\
+                      
+                      <div class="movein">
+                      <tr class="TableInfo">
+                        <th scope="row"></th>
+                        <td class="mois"><p>December</p></td>
+                        <td></td>
+                        <td class="mois"><p>December</p></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                    </div>
+                      <tr class="TableInfo">
+                        <th scope="row"></th>
+                        <td >Buffer raised a small
+                            seed round of $330,000,
+                            to bring total funding to
+                            $450,000 <br>
+                        <a href="">Read more</a>
+                    </td>
+                        <td></td>
+                        <td>
+                            We reached 1 Million
+                            users <br>
+                            <a href="">Read more</a>
+                            <div class="dotted">dbfb</div>
+                            <div class="dotted">dbfb</div>
+                        </td>
+                      </tr>
+        
+                      <tr class="TableInfo Tdsmaller">
+                        <th scope="row"></th>
+                        <td></td>
+                        <td></td>
+                        <td class="mois"><p>December</p></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr class="TableInfo">
+                        <th scope="row"></th>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            We published the whole companies salaries
+                            <br> 
+                            <a href="">Read more</a>
+                        </td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+        
+                </tbody>
+            </table>
+        
+            <div class="rotatedtable" role="region" tabindex="0">
+                <table>
+                    <thead>
+                        <tr class="TableInfo">
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="TableInfo upper">
+                            <td class="mois"><p>November</p></td>
+                        </tr>
+                        <tr class="TableInfo longer">
+                            <td>  Buffer was created as
+                                part of a Startup Sprint <br>
+                            <a href="">Read more</a></td>
+                        </tr>
+                        <tr class="spacer">
+                            <!-- Spacer row without content -->
+                        </tr>
+                        <tr class="TableInfo upper">
+                            <td class="mois"><p>August</p></td>
+                        </tr>
+                        <tr class="TableInfo longer">
+                            <td>
+                                Buffer was accepted into
+                                AngelPad startup
+                                accelerator, with initial
+                                $120,000 investment <br>
+                                <a href="">Read more</a>
+                            </td>
+                        </tr>
+                        <tr class="spacer">
+                            <!-- Spacer row without content -->
+                        </tr>
+                        <tr class="TableInfo up">
+                            <td class="mois"><p>January</p></td>
+                        </tr>
+                        <tr class="TableInfo">
+                            <td>
+                                Our founders had visa troubles
+                                 and started traveling while growing 
+                                 Buffer, the team grew to 
+                                 7 people by August
+                                <br>
+                                <a href="">Read more</a>
+                            </td>
+                        </tr>
+                        <tr class="spacer">
+                            <!-- Spacer row without content -->
+                        </tr>
+                        <tr class="TableInfo up">
+                            <td class="mois"><p>August</p></td>
+                        </tr>
+                        <tr class="TableInfo uppers">
+                            <td>
+                                Buffer's first all-company 
+                                retreat took place in Lake Tahoe
+                                <br>
+                                <a href="">Read more</a>
+                            </td>
+                        </tr>
+                        <tr class="TableInfo">
+                            <td class="mois"><p>December</p></td>
+                        </tr>
+                        <tr class="TableInfo">
+                            <td>
+                                We raised $3.5M in Series A funding
+        
+                                <br>
+                                <a href="">Read more</a>
+                            </td>
+                        </tr>
+                        <tr class="spacer">
+                            <!-- Spacer row without content -->
+                        </tr>
+                        <tr class="TableInfo down">
+                            <td class="mois"><p>December</p></td>
+                        </tr>
+                        <tr class="TableInfo">
+                            <td class="downer" >
+                                <br>
+                                We acquired Respondly
+        
+                                <br>
+                                <a href="">Read more</a>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+                </div>
+        </div>        
         @else
         <p>Aucune donnée d'historique disponible pour le moment.</p>
         @endif
