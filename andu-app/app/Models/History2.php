e9c0c6d93102081f90885ff9632c9c171b1d65e6
oumarsoumare221 <o.soumare3@isepdiamniadio.edu.sn>
2024-07-11 11:50:28 +0000

    changement historie historie2 et 3 sur les models

--- andu-app/app/Models/History2.php
+++ andu-app/app/Models/History2.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class History2 extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'buffer_timeline_id',
+        'month',
+        'event',
+    ];
+
+    public function bufferTimeline()
+    {
+        return $this->belongsTo(BufferTimeline::class);
+    }
 }
--- andu-app/app/Models/History3.php
+++ andu-app/app/Models/History3.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class History3 extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'buffer_timeline_id',
+        'month',
+        'event',
+    ];
+
+    public function bufferTimeline()
+    {
+        return $this->belongsTo(BufferTimeline::class);
+    }
 }
